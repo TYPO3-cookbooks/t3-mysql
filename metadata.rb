c06caa096e789264990205a8df56b6806ac39b69
Michael Stucki <michael.stucki@typo3.org>
2014-06-24 14:52:29 +0200

    Ignore the mysql.events table (suppress stupid warning)

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for mysql"
-version          "0.1.2"
+version          "0.1.3"
 
 depends "mysql", "= 1.3.0"
