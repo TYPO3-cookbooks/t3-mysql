a5d19e7c262521b7e5fc6f4f99b875471c965cae
Steffen Gebert <steffen.gebert@typo3.org>
2015-12-29 00:57:09 +0100

    Modernize this cookbook, using mysql cookbook 5.6

--- attributes/custom.rb
+++ attributes/custom.rb
@@ -17,92 +17,92 @@
 # limitations under the License.
 #
 
-override['mysql']['bind_address'] = "127.0.0.1"
+default['mysql']['bind_address'] = "127.0.0.1"
 
 #### buffers
 
 # Size of the buffer used for index blocks
-override['mysql']['tunable']['key_buffer_size'] = "16M"
+default['t3-mysql']['tunable']['key_buffer_size'] = "16M"
 
 # Buffer size for sorting indexes
-override['mysql']['tunable']['sort_buffer_size'] = "16M"
+default['t3-mysql']['tunable']['sort_buffer_size'] = "16M"
 
 # Buffer size for reading data
-override['mysql']['tunable']['read_buffer_size'] = "4M"
+default['t3-mysql']['tunable']['read_buffer_size'] = "4M"
 
 # Buffer size for reading data which needs to be sorted afterwards
-override['mysql']['tunable']['read_rnd_buffer_size'] = "8M"
+default['t3-mysql']['tunable']['read_rnd_buffer_size'] = "8M"
 
 # Buffer size for joins
-override['mysql']['tunable']['join_buffer_size'] = "2M"
+default['t3-mysql']['tunable']['join_buffer_size'] = "2M"
 
 
 #### innodb
 
 # Limit the number of InnoDB threads
-override['mysql']['tunable']['innodb_thread_concurrency'] = 16
+default['t3-mysql']['tunable']['innodb_thread_concurrency'] = 16
 
 # Memory pool size for data dictionary and other internal data structures
-override['mysql']['tunable']['innodb_additional_mem_pool_size'] = "16M"
+default['t3-mysql']['tunable']['innodb_additional_mem_pool_size'] = "16M"
 
 # Flush method for data and log files
-override['mysql']['tunable']['innodb_flush_method'] = "O_DIRECT"
+default['t3-mysql']['tunable']['innodb_flush_method'] = "O_DIRECT"
 
 # InnoDB buffer pool size
 # This value depends on the size of the InnoDB storage
-override['mysql']['tunable']['innodb_buffer_pool_size'] = "1G"
+default['t3-mysql']['tunable']['innodb_buffer_pool_size'] = "1G"
 
 # Use one InnoDB file per table
-override['mysql']['tunable']['innodb_file_per_table'] = 1
+default['t3-mysql']['tunable']['innodb_file_per_table'] = 1
 
 
 #### logging
 
-override['mysql']['tunable']['log_slow_queries']     = "/var/log/mysql/slow.log"
-override['mysql']['tunable']['long_query_time']      = 2
+default['t3-mysql']['tunable']['log_slow_queries']     = "/var/log/mysql/slow.log"
+default['t3-mysql']['tunable']['long_query_time']      = 2
 
 
 #### myisam
 
 # MyISAM storage engine recovery mode
-override['mysql']['tunable']['myisam-recover'] = "BACKUP,FORCE"
+default['t3-mysql']['tunable']['myisam-recover'] = "BACKUP,FORCE"
 
 # Buffer size for sorting MyISAM indexes
-override['mysql']['tunable']['myisam_sort_buffer_size'] = "64M"
+default['t3-mysql']['tunable']['myisam_sort_buffer_size'] = "64M"
 
 
 #### other-tuning
 
 # Maximum number of simultaneous connections
-override['mysql']['tunable']['max_connections'] = 100
+default['t3-mysql']['tunable']['max_connections'] = 100
 
 # Threads to cache for reuse
-override['mysql']['tunable']['thread_cache_size'] = 50
+default['t3-mysql']['tunable']['thread_cache_size'] = 50
 
 # Do not cache results that are larger than this size
-override['mysql']['tunable']['query_cache_limit'] = "2M"
+default['t3-mysql']['tunable']['query_cache_limit'] = "2M"
 
 # Size of the query cache
-override['mysql']['tunable']['query_cache_size'] = "256M"
+default['t3-mysql']['tunable']['query_cache_size'] = "256M"
 
 # Number of open tables for all threads
-override['mysql']['tunable']['table_cache'] = 1024
+default['t3-mysql']['tunable']['table_cache'] = 1024
 
 # Maximum length for the GROUP_CONCAT function
-override['mysql']['tunable']['group_concat_max_len'] = "32K"
+default['t3-mysql']['tunable']['group_concat_max_len'] = "32K"
 
 # Inactivity timeout
-override['mysql']['tunable']['wait_timeout'] = 300
+default['t3-mysql']['tunable']['wait_timeout'] = 300
 
-override['mysql']['tunable']['max_allowed_packet']   = "16M"
+default['t3-mysql']['tunable']['max_allowed_packet']   = "16M"
 
 ##### temporary data
 
 # Maximum size to which user-created MEMORY tables are permitted to grow
-override['mysql']['tunable']['max_heap_table_size']  = "32M"
+default['t3-mysql']['tunable']['max_heap_table_size']  = "32M"
 
 
-override['mysql']['tunable']['thread_cache']         = "128"
-override['mysql']['tunable']['thread_cache_size']    = 8
-override['mysql']['tunable']['thread_concurrency']   = 10
-override['mysql']['tunable']['thread_stack']         = "256K"
+default['t3-mysql']['tunable']['thread_cache']         = "128"
+default['t3-mysql']['tunable']['thread_cache_size']    = 8
+default['t3-mysql']['tunable']['thread_concurrency']   = 10
+default['t3-mysql']['tunable']['thread_stack']         = "256K"
--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,5 @@ license          "Apache 2.0"
 description      "A wrapper cookbook for mysql"
 version          "0.1.3"
 
-depends "mysql", "= 1.3.0"
+depends "mysql",    "= 5.6.3"
+depends "database", "= 2.3.1"
