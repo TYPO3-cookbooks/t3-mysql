6a7432df6ccfb85c608c508881a596e5a6c6fc4b
Michael Stucki <michael.stucki@typo3.org>
2016-11-12 20:04:52 +0100

    Bump version to 5.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for mysql"
-version          "5.0.0"
+version          "5.1.0"
 
 depends "mysql",    "~> 5.7.0"
 depends "database", ">= 6.0.0"
