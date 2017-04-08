4b54f4305c2131c8185b9f990e1e1fd245178111
Michael Stucki <michael.stucki@typo3.org>
2017-04-08 18:50:08 +0200

    [TASK] Switch to Jenkins v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A wrapper cookbook for mysql"
-version          "5.1.0"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends "mysql",    "~> 5.7.0"
 depends "database", ">= 6.0.0"
